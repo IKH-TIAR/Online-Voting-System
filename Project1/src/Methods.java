9b3f1e3a5b0fb9bd31a7f92ea60c61b8d36aed58
IKH-TIAR <110125914+IKH-TIAR@users.noreply.github.com>
2022-12-07 01:02:13 +0600

    Delete Project1 directory

--- Project1/src/Methods.java
+++
@@ -1,7 +0,0 @@
-public abstract class Methods {
-    abstract void stMenu();
-    abstract void register();
-    abstract void login();
-    abstract void studentMenu(String username);
-    abstract void vote(String username);
-}
