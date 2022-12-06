a3c0fd8d14f3dd40368d053a2eb835814fa7e02d
IKH-TIAR <110125914+IKH-TIAR@users.noreply.github.com>
2022-12-07 01:03:29 +0600

    Add files via upload

---
+++ Project1/src/Methods.java
@@ -0,0 +1,7 @@
+public abstract class Methods {
+    abstract void stMenu();
+    abstract void register();
+    abstract void login();
+    abstract void userMenu(String username);
+    abstract void vote(String username);
+}
